371f85ddb821f7d57f2f42f44c4931fd5ac6fc5a
seongchan <tjdcks0218@naver.com>
2025-09-23 15:39:56 +0900

    chore:패키지   ë° 생성

--- coffee/src/main/java/com/coffee/product/controller/ProductController.java
+++ coffee/src/main/java/com/coffee/product/controller/ProductController.java
@@ -1,8 +1,16 @@
 package com.coffee.product.controller;
 
 
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("")
+@Tag(name = "", description = "")
 public class ProductController {
+
+
 }
--- coffee/src/main/java/com/coffee/product/entity/Product.java
+++ coffee/src/main/java/com/coffee/product/entity/Product.java
@@ -4,12 +4,16 @@ package com.coffee.product.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Product {
-
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
