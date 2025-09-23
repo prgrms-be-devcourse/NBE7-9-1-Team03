a443418b503b9b3db43c1828f8af87539635a0d0
gksdud1109 <vectorh532@gmail.com>
2025-09-23 14:00:28 +0900

    init

---
+++ coffee/src/main/java/com/coffee/global/initData/BaseInitData.java
@@ -0,0 +1,35 @@
+package com.coffee.global.initData;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configuration
+@RequiredArgsConstructor
+public class BaseInitData {
+
+    @Autowired
+    @Lazy
+    private BaseInitData self;
+
+    @Bean
+    ApplicationRunner initDataRunner() {
+        return args -> {
+            self.work1();
+            self.work2();
+        };
+
+    }
+
+    @Transactional
+    public void work1() {
+    }
+
+    @Transactional
+    public void work2() {
+    }
+}
